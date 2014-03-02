cb807f52ecdfcbb100203462e161e1a79cc91928
Arturs Lataks <alataks@redstage.com>
2014-03-02 21:35:13 +0200

    Homepage top menu

---
+++ app/code/local/RLM/Helper/Helper/Data.php
@@ -0,0 +1,34 @@
+<?php
+class RLM_Helper_Helper_Data
+    extends Mage_Core_Helper_Abstract
+{
+    public function getLoginLogoutLink()
+    {
+        $data = array();
+
+        if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+            $data['url'] = Mage::getUrl('customer/account/logout');
+            $data['title'] = $this->__('Log Out');
+        } else {
+            $data['url'] = Mage::getUrl('customer/account/login');
+            $data['title'] = $this->__('Log In');
+        }
+
+        return $data;
+    }
+
+    public function getSettingsUrl()
+    {
+        return Mage::getUrl('customer/account');
+    }
+
+    public function getCartItemsCount()
+    {
+        return Mage::helper('checkout/cart')->getItemsCount();
+    }
+
+    public function getCartUrl()
+    {
+        return Mage::getUrl('checkout/cart');
+    }
+}
--- skin/frontend/rlm/default/js/functions.js
+++ skin/frontend/rlm/default/js/functions.js
@@ -20,7 +20,39 @@ Popup.prototype.init = function (clicked, popup, background, close) {
     background.click(function () {
         popup.find(close).trigger('click');
     });
-}
+};
+
+function TopMenu () {}
+
+TopMenu.prototype.init = function (topMenu, topMenuShow) {
+    topMenu = jQuery(topMenu);
+    topMenuShow = jQuery(topMenuShow);
+    var speed = 500;
+
+    topMenu.find('.hide-button').click(function (e) {
+        e.preventDefault();
+
+        topMenu.animate({
+            top: '-100px'
+        }, speed, function () {
+            topMenuShow.animate({
+                top: '0'
+            }, speed)
+        });
+    });
+
+    topMenuShow.click(function (e) {
+        e.preventDefault();
+
+        topMenuShow.animate({
+            top: '-100px'
+        }, speed, function () {
+            topMenu.animate({
+                top: '0'
+            }, speed)
+        });
+    });
+};
 
 jQuery('document').ready(function () {
     var deliveryInfo = new Popup();
@@ -28,4 +60,7 @@ jQuery('document').ready(function () {
 
     var contactUsForm = new Popup();
     contactUsForm.init('li.contact a', '.popup.contact_us', '.overflow-bg', '.close');
-})
+
+    var topMenu = new TopMenu();
+    topMenu.init('.header-container', '.show-menu');
+});
