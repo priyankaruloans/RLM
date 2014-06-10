ae64a1577e9131fc0479471c62428a358f62f772
Arturs Lataks <arthur.lataks@gmail.com>
2014-06-10 15:32:38 -0400

    Small design fixes over the site

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -19,13 +19,13 @@
 define('DB_NAME', 'rlm');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'rlm');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'rlmpass');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '188.226.202.112');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
