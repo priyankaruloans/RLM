fb917c0c1ce0281a3984e0591fc90d3ead71a5c5
Jevgenijs Fokins <judjinlv@gmail.com>
2014-06-19 21:03:01 +0300

    Testing

--- api.php
+++ api.php
@@ -14,7 +14,7 @@
  *
  * DISCLAIMER
  *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * Do not edit or add to this file if you wish to upgrade Magento to newer .
  * versions in the future. If you wish to customize Magento for your
  * needs please refer to http://www.magentocommerce.com for more information.
  *
